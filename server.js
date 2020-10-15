a32a0c2eb4fdfa0b76c825aeaab08974d90647ea
Keith Tharp <keithstharp@gmail.com>
2020-10-15 14:25:25 -0400

    Setting up server and routes

---
+++ server.js
@@ -0,0 +1,31 @@
+//Require environment variables
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require('morgan');
+
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// all api routes
+const apiRoutes = require("./routes/api-routes");
+app.use("/api", apiRoutes);
+
+// all html routes
+const htmlRoutes = require("./routes/html-routes");
+app.use("", htmlRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mongo-workout-watcher", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
