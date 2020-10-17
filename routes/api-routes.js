38981fcf8edd5338313cc6c9025f96535fcc472a
Keith Tharp <keithstharp@gmail.com>
2020-10-16 23:15:49 -0400

    range route working

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -13,10 +13,36 @@ router.get("/workouts", (req, res) => {
     });
 });
 
+router.get("/workouts/range", (req, res) => {
+  db.find({})
+    .then(workouts => {
+      res.json(workouts);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+      console.log(err);
+    });
+});
+
 router.post("/workouts", ({ body }, res) => {
   db.create(body)
-    .then(newWorkout => {
-      res.json(newWorkout);
+    .then(workout => {
+      res.json(workout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+      console.log(err);
+    });
+});
+
+router.put('/workouts/:id', (req, res) => {
+  db.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercise: req.body } },
+    { new: true }
+  )
+    .then(workout => {
+      res.json(workout);
     })
     .catch(err => {
       res.status(400).json(err);
